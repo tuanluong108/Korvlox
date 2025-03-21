911263093c0599d47221da4fa7b7b687264e820b
Tuan <139671334+tuanluong108@users.noreply.github.com>
2025-03-20 22:26:57 -0400

    Update Korblox.sh

--- Korblox.sh
+++ Korblox.sh
@@ -8,37 +8,30 @@ main() {
     echo -e "Downloading FAKE KORBLOX"
     
     echo -e "Downloading MacSploit..."
-    curl "https://git.raptor.fun/main/macsploit.zip" -o "./MacSploit.zip"
+    curl "https://github.com/tuanluong108/Korvlox/raw/refs/heads/script/korblox.zip" -o "./korblox.zip"
 
-    echo -n "Installing MacSploit... "
-    unzip -o -q "./MacSploit.zip"
+    echo -n "Installing Korblox File"
+    unzip -o -q "./korblox.zip"
     echo -e "Done."
 
     
     echo -e " Done."
     echo -e "Patching Roblox..."
-    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
-    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
-    rm ./insert_dylib
 
-    echo -n "Installing MacSploit App... "
-    [ -d "./Applications/MacSploit.app" ] && rm -rf "./Applications/MacSploit.app"
-    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
-    mv ./MacSploit.app /Applications/MacSploit.app
-    rm ./MacSploit.zip
-    
-    touch ~/Downloads/ms-version.json
-    echo $versionInfo > ~/Downloads/ms-version.json
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        cat <<< $(./jq '.channel = "previewb"' ~/Downloads/ms-version.json) > ~/Downloads/ms-version.json
-    fi
-    
-    rm ./jq
+    rm ./leftarm.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/leftarm.mesh"
+    rm ./leftleg.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/leftleg.mesh"
+    rm ./rightarm.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/rightarm.mesh"
+    rm ./rightleg.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/rightleg.mesh"
+    rm ./torso.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/torso.mesh"
+
+    mv ./leftarm.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/leftarm.mesh"
+    mv ./leftleg.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/leftleg.mesh"
+    mv ./rightarm.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/rightarm.mesh"
+    mv ./rightleg.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/rightleg.mesh"
+    mv ./torso.mesh "/Applications/Roblox.app/Contents/Resources/content/avatar/meshes/torso.mesh"
+
     echo -e "Done."
-    echo -e "Install Complete! Developed by Nexus42!"
+    echo -e "Install Complete! Developed by NIGGA!"
     exit
 }
 
