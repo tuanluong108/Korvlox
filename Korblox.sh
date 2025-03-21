8816b9736bc686af635c565e85311ad6e95ecfed
Tuan <139671334+tuanluong108@users.noreply.github.com>
2025-03-20 22:18:57 -0400

    Update Korblox.sh

--- Korblox.sh
+++ Korblox.sh
@@ -2,81 +2,11 @@
 
 main() {
     clear
-    echo -e "Welcome to the MacSploit Experience!"
-    echo -e "Install Script Version 2.6"
+    echo -e "Welcome to the FAKE!"
 
-    echo -ne "Checking License..."
-    curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
-    chmod +x ./jq
-    
-    curl -s "https://git.raptor.fun/sellix/hwid" -o "./hwid"
-    chmod +x ./hwid
-    
-    local user_hwid=$(./hwid)
-    local hwid_info=$(curl -s "https://git.raptor.fun/api/whitelist?hwid=$user_hwid")
-    local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
-    rm ./hwid
-    
-    if [ "$hwid_resp" != "true" ]
-    then
-        echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
-        read input_key
 
-        echo -n "Contacting Secure Api... "
-        
-        local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-        echo -e "Done.\n$resp"
-        
-        if [ "$resp" != 'Key Activation Complete!' ]
-        then
-            rm ./jq
-            exit
-            return
-        fi
-    else
-        local free_trial=$(echo $hwid_info | ./jq -r ".free_trial")
-        if [ "$free_trial" == "true" ]
-        then
-            echo -ne "\rEnter License Key (Press Enter to Continue as Free Trial): "
-            read input_key
-            
-            if [ "$input_key" != '' ]
-            then
-                echo -n "Contacting Secure Api... "
-                
-                local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-                echo -e "Done.\n$resp"
-            fi
-        else
-            echo -e " Done.\nWhitelist Status Verified."
-        fi
-    fi
-
-    echo -e "Downloading Latest Roblox..."
-    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-    local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
-    local versionInfo=$(curl -s "https://git.raptor.fun/main/version.json")
+    echo -e "Downloading FAKE KORBLOX"
     
-    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
-    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
-    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
-    
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
-    else
-        curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
-    fi
-    
-    echo -n "Installing Latest Roblox... "
-    [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
-    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
-
-    unzip -o -q "./RobloxPlayer.zip"
-    mv ./RobloxPlayer.app /Applications/Roblox.app
-    rm ./RobloxPlayer.zip
-    echo -e "Done."
-
     echo -e "Downloading MacSploit..."
     curl "https://git.raptor.fun/main/macsploit.zip" -o "./MacSploit.zip"
 
@@ -84,13 +14,6 @@ main() {
     unzip -o -q "./MacSploit.zip"
     echo -e "Done."
 
-    echo -n "Updating Dylib..."
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        curl -Os "https://git.raptor.fun/preview/macsploit.dylib"
-    else
-        curl -Os "https://git.raptor.fun/main/macsploit.dylib"
-    fi
     
     echo -e " Done."
     echo -e "Patching Roblox..."
